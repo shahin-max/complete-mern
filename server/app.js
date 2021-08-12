aabbb5b68c33a1381d507814e47f48af4d99216f
shahin-max <humshaheen786@gmail.com>
2021-08-12 20:00:36 +0530

    first commit

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const dotenv = require("dotenv");
 const mongoose = require('mongoose');
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
 const app = express();
+app.use(cookieParser());
 
 dotenv.config({ path: './config.env' });
 
@@ -13,35 +16,34 @@ app.use(express.json());
 // we link the router files to make our route easy 
 app.use(require('./router/auth'));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 
-// Middelware 
-const middleware = (req,res, next) => {
-    console.log(`Hello my Middleware`);
-    next();
-}
 
-// app.get('/', (req, res) => {
-//     res.send(`Hello world from the server app.js`);
+//// I just comment out the below about section 
+// app.get('/about', (req, res) => {
+//     console.log(`Hello my About`);
+//     res.send(`Hello About world from the server`);
 // });
 
-app.get('/about', middleware, (req, res) => {
-    console.log(`Hello my About`);
-    res.send(`Hello About world from the server`);
-});
+// app.get('/contact', (req, res) => {
+//     // res.cookie("Test", 'thapa');
+//     res.send(`Hello Contact world from the server`);
 
-app.get('/contact', (req, res) => {
-    res.send(`Hello Contact world from the server`);
-});
+// });
 
-app.get('/signin', (req, res) => {
-    res.send(`Hello Login world from the server`);
-});
+// app.get('/signin', (req, res) => {
+//     res.send(`Hello Login world from the server`);
+// });
 
 app.get('/signup', (req, res) => {
     res.send(`Hello Registration world from the server`);
 });
+
+//3:step heroku
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static("client/build"));
+}
 
 app.listen(PORT, () => {
     console.log(`server is runnig at port no ${PORT}`);
